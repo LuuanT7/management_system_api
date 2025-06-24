04476031bf07594a2b5d923f4d33fba56bc57006
Alisson <alisontattoskate@gmail.com>
2025-06-23 23:03:59 -0300

    mais uns erros

--- dist/shared/container/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-require("reflect-metadata");
-const PrismaClassRepository_1 = require("../../../src/modules/class/repositories/implementations/PrismaClassRepository");
-tsyringe_1.container.registerSingleton('ClassRepository', PrismaClassRepository_1.PrismaClassRepository);
