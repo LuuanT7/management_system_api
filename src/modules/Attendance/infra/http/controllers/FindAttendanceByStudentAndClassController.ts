0eb3a443b9e150f4c26c401acae40f7321b628b8
Danilo8922 <danlenardial@gmail.com>
2025-06-29 23:55:27 -0300

    Módulo attendance com todas as rotas atualizadas

--- src/modules/Attendance/infra/http/controllers/FindAttendanceByStudentAndClassController.ts
+++ src/modules/Attendance/infra/http/controllers/FindAttendanceByStudentAndClassController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-import { FindAttendanceByClassUseCase } from "../../../UseCases/FindAttendanceByClassUseCases";
+import { FindByIdAttendanceUseCase } from "../../../UseCases/FindByStudentAndClassUsecase";
 
-export class FindAttendanceByClassController {
+export class FindAttendanceByStudentAndClassController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { classId } = req.params;
+    const { studentId, classId } = req.params;
 
-    const useCase = container.resolve(FindAttendanceByClassUseCase);
-    const result = await useCase.execute(classId);
+    const useCase = container.resolve(FindByIdAttendanceUseCase);
+    const result = await useCase.execute(studentId, classId);
 
     return res.status(200).json(result);
   }
 }
+
+
 
