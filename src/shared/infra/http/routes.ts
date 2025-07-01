3878b33d3d175603815638d2d53ff0da5c51ed41
Alisson <alisontattoskate@gmail.com>
2025-06-30 21:32:25 -0300

    inserting Class routes

--- src/shared/infra/http/routes.ts
+++ src/shared/infra/http/routes.ts
@@ -1,18 +1,17 @@
 import { Router } from 'express';
 import { authRoutes } from '@modules/auth/infra/http/routes/auth.routes';
 import { userRoutes } from '@modules/Users/Infra/Http/Routes/user.routes';
-
 import { profileRoutes } from '@modules/Profile/infra/http/routes/profile.routes';
 import { attendanceRoutes } from '@modules/Attendance/infra/http/routers/attendance.routes';
-
+import { classRoutes } from '@modules/Class/infra/http/routes/class.routes';
 
 const routes = Router();
 
 routes.use('/auth', authRoutes);
 routes.use('/users', userRoutes);
-
 routes.use('/profile', profileRoutes);
 routes.use('/attendance', attendanceRoutes);
+routes.use('/classes', classRoutes);
 
 
 export { routes };
